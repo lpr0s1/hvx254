43b4c830acf4dcf47cc9d844183515323564774d
lpr0s1 <119435425+lpr0s1@users.noreply.github.com>
2026-02-23 00:47:22 +0100

    Remove comments from config.js
    Removed comments about payment URL and free limit context.

--- config.js
+++ config.js
@@ -5,9 +5,6 @@
 const DOX_CONFIG = {
     // 1 Seul scan gratuit : On donne juste assez pour créer l'addiction
     FREE_LIMIT: 1,
-
-    // URL du paiement (Stripe, etc.) réglé à 9,99€
-    // Jean-Michel paye car il veut savoir qui le stalke à 23h.
     PAYMENT_URL: "https://buy.stripe.com/cNi7sLe1r6uV5fJanX6wE0h",
 
     // Version
