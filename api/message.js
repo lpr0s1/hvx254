30073402fdfbed9b2fdfae240eaa6699c775eb5e
lpr0s1 <119435425+lpr0s1@users.noreply.github.com>
2026-06-10 15:54:53 +0200

    Update message.js

--- api/message.js
+++ api/message.js
@@ -1,29 +1,27 @@
 let lastMessage = "Aucun message pour le moment";
 
 export default function handler(req, res) {
-  // GET → renvoyer le dernier message reçu
   if (req.method === "GET") {
     return res.status(200).json({
-      status: "OK",
+      status: "Disponible",
       message: lastMessage
     });
   }
 
-  // POST → recevoir un message
   if (req.method === "POST") {
     const { message } = req.body || {};
 
     if (!message) {
-      return res.status(400).json({ error: "Aucun message reçu" });
+      return res.status(400).json({ error: "Aucun message..." });
     }
 
-    lastMessage = message; // on sauvegarde le message
+    lastMessage = message;
 
     return res.status(200).json({
-      status: "OK",
+      status: "Disponible",
       received: message
     });
   }
 
-  return res.status(405).json({ error: "Méthode non autorisée" });
+  return res.status(405).json({ error: "405" });
 }
